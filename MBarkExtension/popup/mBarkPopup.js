509f14687e40096a78b22de16f0f75228cc4ac76
Sam G <samgonza@umich.edu>
2020-06-01 00:19:56 -0400

    Setup global courses

--- MBarkExtension/popup/mBarkPopup.js
+++ MBarkExtension/popup/mBarkPopup.js
@@ -1,4 +1,87 @@
 
+class Course {
+	constructor(name, credits) {
+		this.name = name;
+		this.credits = credits;
+		this.creditsCompleted = 0;
+	}
+}
+gCourses = undefined;
+
+function SaveCourses() {
+	chrome.storage.sync.set({'gCourses': gCourses}, function() {
+		console.log("saved courses");
+	});
+} 
+
+function ResetMemory(callback) {
+	chrome.storage.sync.clear(function() {
+		console.log("Reset Memory");
+		if(typeof callback !== 'undefined') callback();
+	});
+}
+
+function InitCourses(onLoadCallback) {
+
+	// try to load from storage or create a blank one on fail
+
+    chrome.storage.sync.get(['gCourses'], function(result) {
+
+    	if(result.gCourses) {
+
+    		gCourses = result.gCourses;
+    		console.log("Loaded courses from storage");
+
+    	} else {
+			gCourses = [
+				new Course("ENG 100", 4),
+				new Course("ENG 101", 4),
+				new Course("CHEM 125", 1),
+				new Course("CHEM 126", 1),
+				new Course("CHEM 130", 3),
+				new Course("PHYS 140", 4),
+				new Course("PHYS 141", 1),
+				new Course("PHYS 240", 4),
+				new Course("PHYS 241", 1),
+				new Course("MATH 115", 4),
+				new Course("MATH 116", 4),
+				new Course("MATH 214", 4),
+				new Course("MATH 215", 4),
+				new Course("EECS 203", 4),
+				new Course("EECS 280", 4),
+				new Course("EECS 281", 4),
+				new Course("EECS 370", 4),
+				new Course("EECS 376", 4),
+				new Course("EECS 496", 2),
+				new Course("STATS 250", 3),
+				new Course("TCHNCLCM 300", 1),
+				new Course("TCHNCLCM 497", 2)
+			];
+
+			SaveCourses();    	
+			console.log("Created default courses");
+    	}
+
+		if(typeof onLoadCallback !== 'undefined') onLoadCallback();
+    });
+}
+
+function InitPopupPage() {
+
+	var table = document.getElementById("creditTable");
+
+	for(var i = 0; i < gCourses.length; ++i) {
+
+		var row = document.createElement("tr"),
+			course = gCourses[i];
+
+		row.innerHTML = "<td class='reqCourse'>"+course.name+"</td><td>"+course.creditsCompleted+"</td><td>"+course.credits+"</td>"
+		table.appendChild(row);
+	}
+
+	console.log("Init creditTable");
+}
+
 function InitLSASearch() {
 
 	var requiredCourses = document.getElementsByClassName("reqCourse");
@@ -8,8 +91,6 @@ function InitLSASearch() {
 		var registerCourseElemt = requiredCourses[i], 
 			registerCourseName = registerCourseElemt.innerText;
 		
-		console.log(registerCourseElemt);
-
 		registerCourseElemt.addEventListener("click", function(e) {
 			
 			var clickedCourseName = e.target.innerText;
@@ -44,15 +125,21 @@ function InitLSASearch() {
 
 		});
 
-		console.log("Registering required course: "+registerCourseName);
 	}
 
+	console.log("Init LSA Search");
 }
 
 window.addEventListener("load", function(e) { 
 	console.log("Popup Ready!");
 
-	InitLSASearch();
+	InitCourses(function() {
+
+		InitPopupPage();
+		InitLSASearch();
+
+		SaveCourses();
+	});
 
 
 });
