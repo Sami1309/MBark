e2da1fd98199ebbe2272fb006574bc05b7965a2d
Sam G <samgonza@umich.edu>
2020-06-03 23:44:58 -0400

    Added student structure

--- MBarkExtension/popup/mBarkPopup.js
+++ MBarkExtension/popup/mBarkPopup.js
@@ -1,14 +1,109 @@
 // const log = chrome.extension.getBackgroundPage().console.log; //Note(Sam G): This failed to work for me?
 const log = console.log;
 
+const CourseCategories = {
+	kCommon: 				"Common Requirements",
+	kIntellectualBreadth: 	"Intellectual Breadth",
+	kMajorCore: 			"CS Major Core",
+	
+	kULCS: 					"Upper Level CS",
+	kFlexTech: 				"CS Technical Elective",
+	kGenElective: 			"General Elective",
+};
+
+const kSentinel = "|";
+
 class Course {
-	constructor(name, credits) {
-		this.name = name;
+	constructor(category, name, credits) {
+
+		this.category = category; 
+		this.name = name; 		//Math, phys
 		this.credits = credits;
-		this.creditsCompleted = 0;
+
+		this.grade = "";
+		this.numberOfTimesAttempted = 0;
+		this.passed = false;
 	}
 }
+
+class VirtualCourse {
+	constructor(category, distributionReq, courseLevel, credits) {
+		this.category = category;
+		this.distributionReq = distributionReq;
+		this.courseLevel = courseLevel;
+		this.credits = credits;
+	}
+}
+
+class Student {
+
+	constructor(coursesRequiredForGraduation) {
+
+		this.coursesTake = [];
+		this.coursesInProgress = [];
+		this.coursesNeededForGraduation = coursesRequiredForGraduation;
+	
+		this.creditsTowardProgram = 0;
+		this.creditsInProgress = 0;	
+		this.residentClasses = 0;
+		this.cumulativeGPA = 0;
+		this.coreGPA = 0;
+	}
+
+	CanTakeCourse(courseName) {
+		return  {
+			canTake: false,
+			reason: "Not Offered"
+		};
+	}
+
+	GetCourses(courseCategory) {
+		return [];
+	}
+
+	CoreGPAMeet() {
+		return false;
+	}
+
+	CummulativeGPAMeet() {
+		return false;
+	}
+
+	ResidencyReqMeet() {
+		return false;
+	}
+
+	CSResidencyRequirementMeet() {
+		return false;
+	}
+
+	CreditsTowardsProgramMeet() {
+		return false;
+	}
+
+	CumulativeCreditsMeet() {
+		return false;
+	}
+
+
+}
+
 gCourses = undefined;
+gStudent = undefined; //TODO: load this from memory
+
+function GetPreReqs(courseName) {
+	return []; //return list of valid Courses
+}
+
+const CourseOpenStatus = {
+	kOpen: 			"Open",
+	kClosed: 		"Closed",
+	kWeightList: 	"WeightListed"
+};
+
+function IsOpen(course) {
+	return CourseOpenStatus.kClosed;
+}
 
 function SaveCourses() {
 	chrome.storage.sync.set({'gCourses': gCourses}, function() {
@@ -23,9 +118,10 @@ function ResetMemory(callback) {
 	});
 }
 
+
 async function ParsePDF(path, onComplete) {
 	
-	const kSentinel = "|";	// Note: used to separate text groups in a text block
+		// Note: used to separate text groups in a text block
 	this.textBlocks = [];
 
 	// strip all the text block from the pdf
@@ -73,72 +169,82 @@ async function ParsePDF(path, onComplete) {
 	if(onComplete) onComplete(this);
 }
 
-function InitCourses(onLoadCallback) {
-
-	gInfo = {};
-
-	ParsePDF("../files/auditSamG.pdf", function(info) {
-
-		gInfo = info;
-
-	});
+function InitStudent(onLoadCallback) {
 
 	// try to load from storage or create a blank one on fail
-    chrome.storage.sync.get(['gCourses'], function(result) {
+    chrome.storage.sync.get(['gStudent'], function(result) {
 
-    	if(result.gCourses) {
+    	if(result.gStudent) {
 
-    		gCourses = result.gCourses;
-    		console.log("Loaded courses from storage");
+    		gStudent = result.gCourses;
+    		console.log("Loaded gStudent from storage");
 
     	} else {
-			gCourses = [
-				new Course("ENG 100", 4),
-				new Course("ENG 101", 4),
-				new Course("CHEM 125", 1),
-				new Course("CHEM 126", 1),
-				new Course("CHEM 130", 3),
-				new Course("PHYS 140", 4),
-				new Course("PHYS 141", 1),
-				new Course("PHYS 240", 4),
-				new Course("PHYS 241", 1),
-				new Course("MATH 115", 4),
-				new Course("MATH 116", 4),
-				new Course("MATH 214", 4),
-				new Course("MATH 215", 4),
-				new Course("EECS 203", 4),
-				new Course("EECS 280", 4),
-				new Course("EECS 281", 4),
-				new Course("EECS 370", 4),
-				new Course("EECS 376", 4),
-				new Course("EECS 496", 2),
-				new Course("STATS 250", 3),
-				new Course("TCHNCLCM 300", 1),
-				new Course("TCHNCLCM 497", 2)
-			];
+			gStudent = new Student([
+				new Course(CourseCategories.kCommon, "MATH 115", 4),
+				new Course(CourseCategories.kCommon, "MATH 116", 4),
+				new Course(CourseCategories.kCommon, "MATH 214", 4),
+				new Course(CourseCategories.kCommon, "MATH 215 "+kSentinel+" MATH 216", 4),
+				
+				new Course(CourseCategories.kCommon, "ENGR 100", 3),
+				new Course(CourseCategories.kCommon, "ENGR 101 "+kSentinel+" ENGR 151", 4),
+				
+				new Course(CourseCategories.kCommon, "CHEM 125 "+kSentinel+" CHEM 126", 1),
+				new Course(CourseCategories.kCommon, "CHEM 130", 3),
+				
+				new Course(CourseCategories.kCommon, "PHYSICS 140", 4),
+				new Course(CourseCategories.kCommon, "PHYSICS 141", 1),
+				new Course(CourseCategories.kCommon, "PHYSICS 240", 4),
+				new Course(CourseCategories.kCommon, "PHYSICS 241", 1),
+			
+
+				new Course(CourseCategories.kMajorCore, "EECS 203", 4),
+				new Course(CourseCategories.kMajorCore, "EECS 280", 4),
+				new Course(CourseCategories.kMajorCore, "EECS 281", 4),
+				new Course(CourseCategories.kMajorCore, "EECS 370", 4),
+				new Course(CourseCategories.kMajorCore, "EECS 376", 4),
+				new Course(CourseCategories.kMajorCore, "EECS 496", 1),
+				new Course(CourseCategories.kMajorCore, "TCHNCLCM 300", 1),
+				new Course(CourseCategories.kMajorCore, "TCHNCLCM 497", 1),
+				new Course(CourseCategories.kMajorCore, "STATS 250 "+kSentinel+" STATS 412"+kSentinel+" STATS 426"+kSentinel+" IOE 265"+kSentinel+" EECS 301", 4),
+				new Course(CourseCategories.kMajorCore, "EECS 441 "+kSentinel+" EECS 467 "+kSentinel+" EECS470 "+kSentinel+" EECS 473 "+kSentinel+" EECS 480 "+kSentinel+" EECS 494 "+kSentinel+" EECS 495"+kSentinel+" EECS 497", 4),
+				
+
+				new VirtualCourse(CourseCategories.kIntellectualBreadth, "HU", "100+", 3),
+				new VirtualCourse(CourseCategories.kIntellectualBreadth, "HU "+kSentinel+" SS", "300+", 3),
+				new VirtualCourse(CourseCategories.kIntellectualBreadth, "HU "+kSentinel+" SS", "100+", 16), //NOTE: This technically can be fulfilled with PCDC: https://bulletin.engin.umich.edu/ug-ed/reqs/#subnav-14
+	
+				new VirtualCourse(CourseCategories.kULCS, "Upper Level CS", "100+", 16),
+				new VirtualCourse(CourseCategories.kFlexTech, "FlexTech", "100+", 10),
+				new VirtualCourse(CourseCategories.kGenElective,  "General Elective", "100+", 12), //TODO: Sam Confirm this
+			]);
 
 			SaveCourses();    	
 			log("Created default courses");
     	}
 
 		if(typeof onLoadCallback !== 'undefined') onLoadCallback();
-    });
+    });	
 }
 
 function InitPopupPage() {
 
 	var table = document.getElementById("creditTable");
 
-	for(var i = 0; i < gCourses.length; ++i) {
+	sum = 0;
+	courses = gStudent.coursesNeededForGraduation;
+	for(var i = 0; i < courses.length; ++i) {
 
 		var row = document.createElement("tr"),
-			course = gCourses[i];
+			course = courses[i];
 
-		row.innerHTML = "<td class='reqCourse'>"+course.name+"</td><td>"+course.creditsCompleted+"</td><td>"+course.credits+"</td>"
+		row.innerHTML = "<td class='reqCourse'>"+(course instanceof VirtualCourse ? course.category : course.name)+"</td><td>"+course.creditsCompleted+"</td><td>"+course.credits+"</td>"
 		table.appendChild(row);
+
+		sum+= course.credits;
 	}
 
-	log("Init creditTable");
+	log("Init creditTable: "+ sum);
 }
 
 function InitLSASearch() {
@@ -240,7 +346,9 @@ function InitLSASearch() {
 window.addEventListener("load", function(e) { 
 	console.log("Popup Ready!");
 
-	InitCourses(function() {
+	ResetMemory();
+
+	InitStudent(function() {
 
 		InitPopupPage();
 		InitLSASearch();
